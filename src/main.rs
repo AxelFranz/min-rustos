bef09f6f686e846fcaad72995eef4df93835695a
AxelFranz <p.axelfranz@gmail.com>
2023-05-31 00:21:49 +0200

    full encapsulation of VGA buffer // Creation of print! and println! macro

--- src/main.rs
+++ src/main.rs
@@ -1,28 +1,20 @@
 #![no_std]
 #![no_main]
 
+mod vga_buff;
+
 use core::panic::PanicInfo;
 
-static HELLO: &[u8] = b"Hello World!";
-
-
 #[panic_handler] // On crée notre panic handler
-fn panic(_info: &PanicInfo) -> ! {
+fn panic(info: &PanicInfo) -> ! {
+    println!("{}",info);
     loop{}
 }
 
 #[no_mangle]
 pub extern "C" fn _start(){
     
-    let vga_buffer = 0xb8000 as *mut u8; // Adresse du VGA Buffer
-
-    for (i,&byte) in HELLO.iter().enumerate() {
-        unsafe {
-            *vga_buffer.offset(i as isize * 2) = byte; // On print la lettre
-            *vga_buffer.offset(i as isize * 2 + 1) = 0xb; // On set la couleur
-        }
-    }
-
+    println!("Coucou {}","Axel");
     loop{}
 }
 

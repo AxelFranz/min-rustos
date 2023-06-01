8bd232fa06290ec449ddd3726966c951357be4db
AxelFranz <p.axelfranz@gmail.com>
2023-06-01 16:41:35 +0200

    refactoring

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,38 @@
 #![no_std]
 #![no_main]
+#![feature(custom_test_frameworks)]
+#![test_runner(test::test_runner)]
+#![reexport_test_harness_main = "test_main"]
 
 mod vga_buff;
+mod serial;
+#[cfg(test)]
+mod test;
+mod panic;
 
-use core::panic::PanicInfo;
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+#[repr(u32)]
+pub enum QemuExitCode {
+    Success = 0x10,
+    Failed = 0x11,
+}
 
-#[panic_handler] // On crée notre panic handler
-fn panic(info: &PanicInfo) -> ! {
-    println!("{}",info);
-    loop{}
+pub fn exit_qemu(exit_code: QemuExitCode) {
+    use x86_64::instructions::port::Port;
+    unsafe {
+        let mut port = Port::new(0xf4);
+        port.write(exit_code as u32);
+    }
 }
 
 #[no_mangle]
 pub extern "C" fn _start(){
     
     println!("Coucou {}","Axel");
+
+    #[cfg(test)]
+    test_main();
+
     loop{}
 }
 

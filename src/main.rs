f9e91e9cb3bdfea71f78cde12826805d112192ac
AxelFranz <p.axelfranz@gmail.com>
2023-06-11 23:35:28 +0200

    did some shit so i had to c/c the code from the blod

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,7 @@
 #![test_runner(min_rustos::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 
-use min_rustos::{println};
-use x86_64::structures::paging::Translate;
+use min_rustos::{println, memory::BootInfoFrameAllocator};
 use core::panic::PanicInfo;
 use bootloader::{BootInfo, entry_point};
 
@@ -16,30 +15,22 @@ fn kernel_main (boot_info: &'static BootInfo) -> ! {
     use min_rustos::memory;
     use x86_64::{VirtAddr};
 
+    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
+    let mut mapper = unsafe { memory::init(phys_mem_offset) };
+    let mut frame_allocator = unsafe { BootInfoFrameAllocator::init(&boot_info.memory_map) };
+
+    // map an unused page
+    let page = Page::containing_address(VirtAddr::new(0xdeadbeaf000));
+    memory::create_example_mapping(page, &mut mapper, &mut frame_allocator);
+
+    // write the string `New!` to the screen through the new mapping
+    let page_ptr: *mut u64 = page.start_address().as_mut_ptr();
+    unsafe { page_ptr.offset(400).write_volatile(0x_f021_f077_f065_f04e) };
+
+
     min_rustos::init();
     println!("Bienvenue");
     println!();
-
-    let phys_mem_offset = VirtAddr::new(boot_info.physical_memory_offset);
-
-    let mapper = unsafe {memory::init(phys_mem_offset)};
-
-    let addresses = [
-        // the identity-mapped vga buffer page
-        0xb8000,
-        // some code page
-        0x201008,
-        // some stack page
-        0x0100_0020_1a10,
-        // virtual address mapped to physical address 0
-        boot_info.physical_memory_offset,
-    ];
-
-    for &address in &addresses {
-        let virt = VirtAddr::new(address);
-        let phys = mapper.translate_addr(virt);
-        println!("{:?} -> {:?}", virt, phys);
-    }
 
 
     #[cfg(test)]
